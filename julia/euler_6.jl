6c18c462cedbd28d54b5b665cffa2f8010036075
Martin Hinsch <hinsch.martin@gmail.com>
2019-11-05 12:00:33 +0000

    more typos and misc errors

--- julia/euler_6.jl
+++ julia/euler_6.jl
@@ -10,14 +10,17 @@ diff_sqs_ssq(n) = squared_sum(n) - sum_squares(n)
 
 # alternative method, less efficient (n(n-1)/2 operations instead of 2n),
 # but might be useful for large numbers where sum of quares would overflow
-function diff_ssq_sqs_alt(n)
+function diff_sqs_ssq_alt(n)
 	sum = 0
 	for i in 1:n, j in 1:(i-1)
 		sum += i*j
 	end
+
+	2*sum
 end
 			
 
 
-println(diff_sqs_ssq(100))
+println("standard method: ", diff_sqs_ssq(100))
+println("alternative method: ", diff_sqs_ssq_alt(100))
 
