fb30b51be810d067b4b049d7affa799bac550b35
Martin Hinsch <hinsch.martin@gmail.com>
2019-11-05 16:15:56 +0000

    typos

--- julia/euler_6.jl
+++ julia/euler_6.jl
@@ -2,14 +2,15 @@
 # square each number, then sum up
 sum_squares(n) = mapreduce(x->x^2, +, 1:n)
 
-# sum up, the square
+# sum up, then square
 squared_sum(n) = reduce(+, 1:n)^2
 
 # calc the diff
 diff_sqs_ssq(n) = squared_sum(n) - sum_squares(n)
 
 # alternative method, less efficient (n(n-1)/2 operations instead of 2n),
-# but might be useful for large numbers where sum of quares would overflow
+# but might be useful for large numbers where sum of squares would overflow
+# (not needed here, but included for fun)
 function diff_sqs_ssq_alt(n)
 	sum = 0
 	for i in 1:n, j in 1:(i-1)
